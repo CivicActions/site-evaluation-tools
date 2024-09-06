693fc87f7b2066d71a1adb9f52059bf2e9af94e0
Kit Lykos <140734297+KitLykos@users.noreply.github.com>
2024-09-06 14:48:33 -0400

    cms config
    Trying to use config

---
+++ .github/workflows/unlighthouse.config-cms.ts
@@ -0,0 +1,42 @@
+module.exports = {
+  site: 'cms.gov',
+  scanner: {
+    include: [
+      "/",
+      "/about",
+      "/foia",
+      "/about-cms/web-policies-important-links/web-policies/privacy",
+      "/search/cms",
+      "/sitemap",
+      "/accessibility",
+      "/contact",
+      "/marketplace/agents-brokers/registration-training",
+      "/about-cms/work-with-us/careers",
+      "/medicare/physician-fee-schedule/search/overview",
+      "/acronyms",
+      "/*",
+    ],
+    exclude: [
+      "/*.pdf",
+      "/*.asp",
+      "/*.aspx",
+      "/sample-pfs-searches",
+      "/security-guidelines-office-location",
+      "/status-indicators",
+      "/blog"
+    ],
+    // run lighthouse for each URL 1 time(s)
+    samples: 3,
+    // use desktop to scan
+    device: 'desktop',
+    // enable the throttling mode
+    throttle: true,
+    // increase the maximum number of routes - https://unlighthouse.dev/api/config#scannermaxroutes
+    maxRoutes: 500,
+    // skip the javascript scan
+    skipJavascript: false,
+    // use sitemaps - arrays are possible for specific sites https://unlighthouse.dev/api/config#scannersitemap
+    sitemap: true,
+  },
+  debug: false,
+};
