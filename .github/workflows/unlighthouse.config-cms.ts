2bf22b551345eefab14376224fcb8652c749a293
Kit Lykos <140734297+KitLykos@users.noreply.github.com>
2024-09-12 10:59:13 -0400

    Update unlighthouse.config-cms.ts
    job seems weird since I messed with config, baselining it again

--- .github/workflows/unlighthouse.config-cms.ts
+++ .github/workflows/unlighthouse.config-cms.ts
@@ -26,13 +26,13 @@ module.exports = {
       "/blog"
     ],
     // run lighthouse for each URL 1 time(s)
-    samples: 1,
+    samples: 3,
     // use desktop to scan
     device: 'desktop',
     // enable the throttling mode
     throttle: true,
     // increase the maximum number of routes - https://unlighthouse.dev/api/config#scannermaxroutes
-    maxRoutes: 10,
+    maxRoutes: 500,
     // skip the javascript scan
     skipJavascript: false,
     // use sitemaps - arrays are possible for specific sites https://unlighthouse.dev/api/config#scannersitemap
